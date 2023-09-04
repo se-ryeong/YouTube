d0bd3a98208a4fad616643d4557e3fb662433d46
= <=>
2023-09-04 17:15:48 +0900

    [FEAT]extension 추가

---
+++ YouTubeApp/YouTubeApp/Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Extension.swift
+//  YouTubeApp
+//
+//  Created by SeoJunYoung on 2023/09/04.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func addPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: self.frame.height))
+        self.leftView = paddingView
+        self.rightView = paddingView
+        self.leftViewMode = ViewMode.always
+        self.rightViewMode = ViewMode.always
+    }
+}
+
+extension CGFloat {
+    static let defaultPadding:CGFloat = 16
+}
