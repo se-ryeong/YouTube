75bea5fae88f1a350f9b8d8a4d1fa2a361d3cf8e
= <=>
2023-09-05 21:22:24 +0900

    [FIX] TabbarController 수정

--- YouTubeApp/YouTubeApp/TabBarController.swift
+++ YouTubeApp/YouTubeApp/TabBarController.swift
@@ -11,8 +11,7 @@ final class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configure()
     }
 
 }
