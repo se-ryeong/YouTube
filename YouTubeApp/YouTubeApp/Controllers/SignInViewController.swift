ec0efe6bf12b465802dc2f98e98a40adf7c8a1fd
= <=>
2023-09-05 10:36:58 +0900

    [FEAT] 키보드 대응

--- YouTubeApp/YouTubeApp/Controllers/SignInViewController.swift
+++ YouTubeApp/YouTubeApp/Controllers/SignInViewController.swift
@@ -15,6 +15,12 @@ final class SignInViewController: UIViewController {
         super.viewDidLoad()
         setUp()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
 }
 
 extension SignInViewController{
@@ -23,7 +29,26 @@ extension SignInViewController{
         self.view.addSubview(signInView)
         signInView.signUpButton.addTarget(self, action: #selector(signUpButtonTapped(_:)), for: .touchUpInside)
         signInView.signInButton.addTarget(self, action: #selector(signInButtonTapped(_:)), for: .touchUpInside)
+        signInView.idTextField.delegate = self
+        signInView.passWordTextField.delegate = self
     }
+    
+    // MARK: - 키보드 대응
+
+    //KeyBoardUp,ViewUp
+    @objc func keyboardUp(notification:NSNotification) {
+        UIView.animate(withDuration: 0.3, animations: { self.signInView.transform = CGAffineTransform(translationX: 0, y: -100) })
+    }
+    
+    //KeyBoardDown,ViewDown
+    @objc func keyboardDown() {
+        self.signInView.transform = .identity
+    }
+    //배경 터치시 키보드 내려가는 메서드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+    }
+    
     // MARK: - ButtonTapped
 
     @objc func signUpButtonTapped(_ sender: UIButton){
@@ -34,4 +59,8 @@ extension SignInViewController{
         print(#function)
     }
     
+}
+
+extension SignInViewController: UITextFieldDelegate{
+
 }
