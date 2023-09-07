9a0c2cde2d38b27ebfd35e5a48eae9c47dabb5f1
hajin <haajin12@gmail.com>
2023-09-07 10:48:43 +0900

    [FEAT]/collectionView 추가

--- YouTubeApp/YouTubeApp/Views/MyPageView.swift
+++ YouTubeApp/YouTubeApp/Views/MyPageView.swift
@@ -7,16 +7,18 @@
 
 import UIKit
 
-final class MyPageView: UIView {
+final class MyPageView: UICollectionViewCell {
     
     private let videoLabel = UILabel()
     private let logOutButton = UIButton()
     private let nickNameButton = UIButton()
     private let profileButton = UIButton()
+    private let profileImageView = UIImageView()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .systemBackground
+        self.backgroundColor = .black
         setUp()
     }
     
@@ -29,15 +31,19 @@ final class MyPageView: UIView {
         setUplogOutButton()
         setUpnickNameButton()
         setUpprofileButton()
+        setUpProfileImageView()
+        
     }
     
     func setUpVideoLabel(){
         videoLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(videoLabel)
         videoLabel.text = "관심있는 동영상"
+        videoLabel.textColor = .white
         //        videoLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        
         NSLayoutConstraint.activate([
-            videoLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 200),
+            videoLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 250),
             videoLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: .defaultPadding),
             videoLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -.defaultPadding),
         ])
@@ -46,7 +52,7 @@ final class MyPageView: UIView {
         logOutButton.translatesAutoresizingMaskIntoConstraints = false
         addSubview(logOutButton)
         logOutButton.setTitle("로그아웃", for: .normal)
-        logOutButton.setTitleColor(UIColor.black, for: .normal)
+        logOutButton.setTitleColor(UIColor.white, for: .normal)
 
         
         NSLayoutConstraint.activate([
@@ -58,11 +64,11 @@ final class MyPageView: UIView {
         nickNameButton.translatesAutoresizingMaskIntoConstraints = false
         addSubview(nickNameButton)
         nickNameButton.setTitle("닉네임", for: .normal)
-        nickNameButton.setTitleColor(UIColor.black, for: .normal)
+        nickNameButton.setTitleColor(UIColor.white, for: .normal)
 
 
         NSLayoutConstraint.activate([
-            nickNameButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 200),
+            nickNameButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 70),
             nickNameButton.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: .defaultPadding),
             nickNameButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -.defaultPadding),
         ])
@@ -71,11 +77,31 @@ final class MyPageView: UIView {
         profileButton.translatesAutoresizingMaskIntoConstraints = false
         addSubview(profileButton)
         profileButton.setTitle("프로필 수정", for: .normal)
-        profileButton.setTitleColor(UIColor.black, for: .normal)
+        profileButton.setTitleColor(UIColor.white, for: .normal)
 
         NSLayoutConstraint.activate([
-            profileButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 200),
+            profileButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 120),
+            profileButton.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: .defaultPadding),
             profileButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -.defaultPadding),
+        ])
+    }
+    func setUpProfileImageView() {
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.image = UIImage(systemName: "person")
+        profileImageView.backgroundColor = .systemIndigo
+        profileImageView.tintColor = .white
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.layer.cornerRadius = 50
+        profileImageView.clipsToBounds = true
+        addSubview(profileImageView)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 50),
+            //topAnchor 위에 기준
+            //bottomAnchor 아래 기준
+            profileImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: .defaultPadding),
+            profileImageView.widthAnchor.constraint(equalToConstant: 100),
+            profileImageView.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
 }
